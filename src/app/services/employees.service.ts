5b36b356522d951a077b58f17dd586914e69be21
Your Name <you@example.com>
2022-10-27 16:07:03 +0530

    first commit

---
+++ src/app/services/employees.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../Models/employee.model';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeesService {
+
+  baseApiUrl: string = environment.baseApiUrl;
+
+  constructor(private http: HttpClient ) { }
+
+  getAllEmployees(): Observable<Employee[]>{
+    return this.http.get<Employee[]>(this.baseApiUrl + '/api/employees')
+  }
+
+  addEmployee(addEmployeeRequest: Employee): Observable<Employee[]>{
+    addEmployeeRequest.id = '00000000-0000-0000-0000-000000000000';
+    return this.http.post<Employee[]>(this.baseApiUrl + '/api/employees', addEmployeeRequest);
+  }
+
+  getEmployee(id: string): Observable<Employee>{
+    return this.http.get<Employee>(this.baseApiUrl + '/api/employees/' + id);
+  }
+
+  updateEmployee(id: string, updateEmployeeRequest: Employee): Observable<Employee>{
+    return this.http.put<Employee>(this.baseApiUrl + '/api/employees/' + id, updateEmployeeRequest);
+  }
+
+  deleteEmployee(id: string): Observable<Employee>{
+    return this.http.delete<Employee>(this.baseApiUrl + '/api/employees/' + id);
+  }
+
+
+
+}
